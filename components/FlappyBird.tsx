76e5743120312ccf941440a52014d75714193d06
moreshk <moreshk@gmail.com>
2024-08-23 16:37:15 +0530

    no logo

--- components/FlappyBird.tsx
+++ components/FlappyBird.tsx
@@ -1,44 +1,17 @@
-import { motion } from "framer-motion";
 import useGame from "../hooks/useGame";
 import useInterval from "../hooks/useInterval";
 import { useState, useEffect } from 'react';
 import BirdTail from './BirdTail';
 
-export function Bird() {
-  const {
-    bird: {
-      size: { height, width },
-      frame,
-      isFlying,
-      flap: { delay },
-    },
-    getNextFrame,
-  } = useGame();
-  useInterval(() => getNextFrame(), isFlying ? delay : null);
-  return (
-    <div
-      style={{
-        backgroundImage: "url(bird.png)",
-        height,
-        width,
-        backgroundPosition: frame,
-        backgroundSize: "auto 100%",
-        zIndex: 100,
-      }}
-    />
-  );
-}
 export default function FlappyBird() {
   const {
     isStarted,
     bird: {
       fall: { delay },
       position,
-      animate,
       size,
     },
     fall,
-    window,
   } = useGame();
 
   const [tailPositions, setTailPositions] = useState<{ x: number; y: number }[]>([]);
@@ -60,27 +33,5 @@ export default function FlappyBird() {
     }
   }, [position, isStarted, size]);
 
-  return (
-    <>
-      <BirdTail positions={tailPositions} />
-      <motion.div
-        className={`m-auto absolute z-40 ${
-          !isStarted && "animate-pulse"
-        } w-20 h-10`}
-        style={{
-          ...position,
-        }}
-        animate={{
-          ...position,
-          ...animate,
-        }}
-        transition={{
-          ease: "linear",
-          duration: 0.25,
-        }}
-      >
-        <Bird />
-      </motion.div>
-    </>
-  );
+  return <BirdTail positions={tailPositions} />;
 }
