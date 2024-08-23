fc1b8082abab50b717c88bc75249a4e6898edf5c
moreshk <moreshk@gmail.com>
2024-08-23 15:11:17 +0530

    one

--- components/Background.tsx
+++ components/Background.tsx
@@ -2,12 +2,8 @@ type Props = {};
 
 export default function Background({}: Props) {
   return (
-    <div className="absolute w-full h-full z-1 pb-2 rounded-t-lg">
-      <img
-        src="bg.png"
-        alt=""
-        className="w-full h-full object-cover  rounded-t-lg"
-      />
+    <div className="absolute w-full h-full z-1 pb-2 rounded-t-lg bg-gray-900">
+      {/* Dark background color */}
     </div>
   );
 }
--- components/Pipes.tsx
+++ components/Pipes.tsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
 import useGame from "../hooks/useGame";
 import useInterval from "../hooks/useInterval";
+import React from 'react';
 
 export default function Pipes() {
   const {
@@ -9,43 +10,61 @@ export default function Pipes() {
     pipes: pipesArray,
     movePipes,
   } = useGame();
+
   useInterval(() => movePipes(), isStarted ? delay : null);
+
   return (
     <>
       {pipesArray.map((pipes, index) => (
-        <>
+        <React.Fragment key={index}>
           <motion.div
             key={pipes.top.key}
             initial={pipes.top.initial}
             animate={pipes.top.position}
             style={{
               ...pipes.top.size,
-              rotate: 180,
+              originY: 1,
             }}
             className="absolute"
-            children={<Pipe />}
             transition={{
               ease: "linear",
             }}
-          />
+          >
+            <Pipe position="top" />
+          </motion.div>
           <motion.div
             key={pipes.bottom.key}
             initial={pipes.bottom.initial}
             animate={pipes.bottom.position}
             style={pipes.bottom.size}
             className="absolute"
-            children={<Pipe />}
             transition={{
               ease: "linear",
             }}
-          />
-        </>
+          >
+            <Pipe position="bottom" />
+          </motion.div>
+        </React.Fragment>
       ))}
     </>
   );
 }
-export function Pipe() {
+
+interface PipeProps {
+  position: 'top' | 'bottom';
+}
+
+export function Pipe({ position }: PipeProps) {
+  const color = position === 'top' ? 'red' : 'green';
+  const borderColor = position === 'top' ? 'darkred' : 'darkgreen';
+
   return (
-    <img src="pipe.png" className="h-full w-full pointer-events-none" alt="" />
+    <div 
+      className={`h-full w-full border-4 rounded-sm`}
+      style={{
+        backgroundColor: color,
+        borderColor: borderColor,
+      }}
+    />
   );
 }
